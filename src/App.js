58dd7261a34b0baa1314ab84bf58c6a464c3ef81
Julia K <123kochygova@gmail.com>
2023-09-29 09:56:28 +0300

    hhj

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import { Funct ,Funct2} from './components/Heder/heder';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <a>мой проект</a>
+      <Funct/>
+      <Funct2/>
     </div>
   );
 }
+
 
 export default App;
