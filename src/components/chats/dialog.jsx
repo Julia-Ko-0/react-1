124788f83ef2e2223f1b1b293779b5f8f26a26c1
Julia K <123kochygova@gmail.com>
2023-10-20 14:56:53 +0300

    ,djfnj,vfzd

--- src/components/chats/dialog.jsx
+++ src/components/chats/dialog.jsx
@@ -1,30 +1,32 @@
 import { CrElDialogName,CrSms,CrPolsName} from "./profile"
 import a from './dialog.module.css'
-// import {Mas} from './masDial.jsx'
+import {Mas} from './masDial.jsx'
 import { useState } from "react"
+// import {Poisk} from './poisk'
 
 
 // console.log(Mas)
 function CrDialogs(){
     
-        const [Mas,SetCount]=useState([{ id:1,
-            name:"name1",
-            img:"",
-            status:"печатает...",
-            sms:"ndsjvnfdsk skdnf"
-        },
-        { id:2,
-            name:"name2",
-            img:"https://www.svgrepo.com/show/530293/tree-2.svg",
-            status:"печатает...",
-            sms:"lkmrgrndsjvnfdsk skdnf"
-        },
-        {id:3,
-            name:"name3",
-            img:"",
-            status:"печатает...",
-            sms:"rkgmlrkes ndsjvnfdsk skdnf"
-        }])
+        // let [Mas,SetCount]=useState([{ id:1,
+        //     name:"name1",
+        //     img:"",
+        //     status:"печатает...",
+        //     sms:"ndsjvnfdsk skdnf"
+        // },
+        // { id:2,
+        //     name:"name2",
+        //     img:"https://www.svgrepo.com/show/530293/tree-2.svg",
+        //     status:"печатает...",
+        //     sms:"lkmrgrndsjvnfdsk skdnf"
+        // },
+        // {id:3,
+        //     name:"name3",
+        //     img:"",
+        //     status:"печатает...",
+        //     sms:"rkgmlrkes ndsjvnfdsk skdnf"
+        // }])
+        const [chat,SetCount]=useState(Mas)
         function DbElMas(){
                 let i1=prompt('name','')
                 let i2=prompt('sms')
@@ -35,7 +37,21 @@ function CrDialogs(){
                         status:"stat",
                         sms:i2
                     }
-                    SetCount([...Mas,newChat])
+                    SetCount([...chat,newChat])
+            }
+            function Poisk(){
+                let impPoisk = document.getElementById('impPoisk')
+                
+                if('' != impPoisk.value){
+                    let newChat = Mas.filter(a=> a.name.includes(impPoisk.value))
+                    console.log(newChat)
+                SetCount([newChat])
+                    
+                    console.log(Mas)
+                }
+                else{console.log('aaaaa')}
+                // SetCount([...Mas,newMas])
+                // SetCount(newChat)
             }
 
     
@@ -50,7 +66,7 @@ function CrDialogs(){
             <div class={a.chatsDial}>
                 <div class = {a.dialogs}>
                     <div>
-                        <input class={a.inp} placeholder="Поиск" ></input>
+                        <input id='impPoisk' class={a.inp} onInput={Poisk} placeholder="Поиск" ></input>
                         <div class = {a.chats}>
                             
                             { 
@@ -59,15 +75,6 @@ function CrDialogs(){
                                     <CrElDialogName pfoto={chat.img} text={chat.sms} name={chat.name}/>
                                 ))
                             }
-                            {/* <div id='msOkn' class={a.modalOkn}>
-                                <input id='inp1' placeholder="name"></input>
-                                <input id='inp2' placeholder="text"></input>
-                                <input id='inp3' placeholder="img"></input>
-                                 <button id="btnModal">создатьЫ</button>
-                            </div> */}
-                            {/* <CrElDialogName text="djhijtd zkdrhgkidf xfcighdiuf xzfhgidzf fuhgid" slk='name1' name='name'pfoto='https://s1.1zoom.ru/big3/474/354282-admin.jpg'/>
-                            <CrElDialogName slk='name2' name='name2'/>
-                            <CrElDialogName slk='name3'name='name3' pfoto='https://www.svgrepo.com/show/530293/tree-2.svg'/> */}
                             
                         </div>
                     </div>
@@ -94,4 +101,4 @@ function CrDialogs(){
 // b.addEventListener('click',()=>{
 //     document.getElementById('msOkn').style.display = 'flex'
 // }
-export{CrDialogs,}
+export{CrDialogs}
